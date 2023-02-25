461d2b8de9fd9ad7d5f8cc4152f1505b8eab39d3
Albert <AccretionDiskThat@Gmail.com>
2023-02-25 21:29:19 +0500

    redirect nginx work sudo git add .sudo git add .

--- Spring/demo/src/main/java/com/example/demo/MvcConfig.java
+++ Spring/demo/src/main/java/com/example/demo/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
-        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/t/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
     }
--- Spring/demo/src/main/java/com/example/demo/WebSecurityConfig.java
+++ Spring/demo/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/t/", "/home").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
